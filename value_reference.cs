edacbcbdb1cb7379b819ba8275369b7f87d82cf2
omerami2804 <58825131+omerami2804@users.noreply.github.com>
2019-12-26 20:27:03 +0200

    Add files via upload

---
+++ value_reference.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstEmployee;
+            string secondEmployee;
+
+            firstEmployee = "Danny Shovevany";
+            secondEmployee = "Yonatan Hakatan";
+
+            //string interpolation = $
+            //Escape characters - \[enter letter here] - gives you a specific command. for example \n means new line 
+            Console.WriteLine($"inside Main Method\n--------\n{firstEmployee}\n{secondEmployee}\n\n");
+
+            //ChangeName(firstEmployee, secondEmployee);
+
+            ChangeName(ref firstEmployee, ref secondEmployee);
+
+            //ChangeName(out string firstEmployee, out string secondEmployee);
+
+            Console.WriteLine($"inside Main Method\n--------\n{firstEmployee}\n{secondEmployee}\n\n");
+        }
+        //Passing by value
+       /* public static void ChangeName(string fEmp, string sEmp)
+        {
+            fEmp = "Geveret Pilpelet";
+            sEmp = "Moosh haShor";
+
+            Console.WriteLine($"Outside Main Method\n--------\n{fEmp}\n{sEmp}\n\n"); 
+            */
+
+          /*  public static void ChangeName(ref string fEmp,ref string sEmp)
+            { //reference takes "address" from existing "ref string"
+                fEmp = "Geveret Pilpelet";
+                sEmp = "Moosh haShor";
+
+                Console.WriteLine($"Outside Main Method\n--------\n{fEmp}\n{sEmp}\n\n");
+                */
+
+            public static void ChangeName( ref string fEmp, ref string sEmp)
+            { //reference takes "address" from existing "ref string"
+                fEmp = "Geveret Pilpelet";
+                sEmp = "Moosh haShor";
+
+                Console.WriteLine($"Outside Main Method\n--------\n{fEmp}\n{sEmp}\n\n");
+            }
+    }
+}
